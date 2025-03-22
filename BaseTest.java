0bb9821a64f0ce9ea337412c2f6cd2d14f100053
Prachi Tomar <148306218+PrachiTomar1@users.noreply.github.com>
2025-03-22 13:55:58 +0530

    Create BaseTest.java
    To avoid redundant code, a Base Class is used to handle setup and teardown.

---
+++ BaseTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
